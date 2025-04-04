3b2ef8c0efa373d6e51981b980b2cbd424466cc9
virutarasewiez <virutarasewiez@users.noreply.github.com>
2025-04-04 11:16:44 -0300

    stable3

--- src/app/contacto/page.tsx
+++ src/app/contacto/page.tsx
@@ -1,35 +1,13 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "@/app/globals.css";
-import Header from "@/components/Header";
-import Oficinavirtual from "@/components/oficinavirtual";
 import Carrusel from "@/components/carrusel";
-import Footer from "@/components/Footer";
+import Oficinavirtual from "@/components/oficinavirtual";
 import Asesoramiento from "@/components/asesoramiento";
-const inter = Inter({ subsets: ["latin"] });
 
-export const metadata: Metadata = {
-  title: "Evolución Seguros",
-  description: "La mejor cobertura para tu tranquilidad",
-};
-
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default function Home() {
   return (
-    <html lang="es">
-      <body className={inter.className}>
-        <Header />
-        <main className="min-h-screen">
-          {children}
-          <Carrusel />
-          <Oficinavirtual />
-          <Asesoramiento />
-        </main>
-        <Footer />
-      </body>
-    </html>
+    <>
+      <Carrusel />
+      <Oficinavirtual />
+      <Asesoramiento />
+    </>
   );
 }
