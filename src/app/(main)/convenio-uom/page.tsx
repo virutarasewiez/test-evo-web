04e1b0a8adf569a673b03c2600159bdbfddc3e70
virutarasewiez <virutarasewiez@users.noreply.github.com>
2025-04-14 11:07:22 -0300

    cambios en pagina uom

--- src/app/(main)/convenio-uom/page.tsx
+++ src/app/(main)/convenio-uom/page.tsx
@@ -38,8 +38,8 @@ export default function ConvenioUOM() {
                 <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z" />
               </svg>
             </div>
-            <p className="text-lg text-center text-white mb-4">
-              <strong>Completá tus datos y los de tus beneficiarios en nuestra app. ¡Es fácil y rápido!</strong>
+            <p className="text-lg text-center text-white mb-0">
+              <strong>Completá tus datos y los de tus beneficiarios en nuestra app. <br />¡Es fácil y rápido!</strong>
             </p>
             <div className="flex items-center gap-2 text-white">
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5">
