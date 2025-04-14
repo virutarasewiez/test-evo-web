10bb821ee573b97a183c3e2bef1d74bd8b311e84
virutarasewiez <virutarasewiez@users.noreply.github.com>
2025-04-14 12:39:12 -0300

    agrande los 9 iconos y lo centre

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -52,13 +52,13 @@ export default function Header() {
           <div className="relative" ref={menuRef}>
             <button
               onClick={() => setIsMenuOpen(!isMenuOpen)}
-              className="focus:outline-none cursor-pointer hover:scale-110 transition duration-300"
+              className="focus:outline-none cursor-pointer hover:scale-115 transition duration-300 flex items-center"
               aria-expanded={isMenuOpen}
               aria-label="Abrir menú"
             >
               <svg
-                width="24"
-                height="24"
+                width="34"
+                height="34"
                 viewBox="0 0 24 24"
                 fill="none"
                 xmlns="http://www.w3.org/2000/svg"
